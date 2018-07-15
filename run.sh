d921e9483ac572f87030c5132d18549f5f7cb031
Andriy Nykyforuk <aqostic@XPUALVIW0709.local>
2018-07-15 20:26:53 +0300

    fix

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-sudo source ~/.bashrc
-nvm install 10
-npm init
-npm install express --save-dev
-node worker.js --host "http://ec2-18-219-93-244.us-east-2.compute.amazonaws.com:3000/link"
+chmod u+x run2.sh
+./run2.sh
