6ca28199402ace2297dc7d45bf02098a608e8a2d
Andriy Nykyforuk <aqostic@XPUALVIW0709.local>
2018-07-15 22:18:57 +0300

    git igonore fix

--- run.sh
+++
@@ -1,3 +0,0 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-chmod u+x run2.sh
-./run2.sh
--- run2.sh
+++
@@ -1,4 +0,0 @@
-nvm install 10
-npm init
-npm install express --save-dev
-node worker.js --host "http://ec2-18-219-93-244.us-east-2.compute.amazonaws.com:3000/link"
